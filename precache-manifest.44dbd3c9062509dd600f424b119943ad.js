58b708d21e4ca3fac0252bc1de61d65ae0fac53b
dungbik <49610681+dungbik@users.noreply.github.com>
2020-07-20 17:18:20 +0900

    Updates

---
+++ precache-manifest.44dbd3c9062509dd600f424b119943ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db60436d9e3075a102f7a8c5d4a9d93f",
+    "url": "/dungbik/react-movie-app/index.html"
+  },
+  {
+    "revision": "5a6e2675edc451e6fcf4",
+    "url": "/dungbik/react-movie-app/static/css/main.8b753a5b.chunk.css"
+  },
+  {
+    "revision": "e722be252d40ed206eab",
+    "url": "/dungbik/react-movie-app/static/js/2.c9e1654d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dungbik/react-movie-app/static/js/2.c9e1654d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a6e2675edc451e6fcf4",
+    "url": "/dungbik/react-movie-app/static/js/main.4bdf5e1c.chunk.js"
+  },
+  {
+    "revision": "ff01fb2b6016e5e4d2ae",
+    "url": "/dungbik/react-movie-app/static/js/runtime-main.02bb592a.js"
+  }
+]);
